806ec58f0f69e0b26bf81139fd110c12e48bd012
obayram34 <oralbayram28@gmail.com>
2023-10-27 22:17:56 +0300

    training added.

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,TrainingConfig,PrepareBaseModelConfig,PrepareCallbacksConfig)
 
 
 
@@ -69,6 +69,34 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(str(self.config.data_ingestion.unzip_dir), "Chicken-fecal-images")
+        create_directories([
+            Path(str(training.root_dir))
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(str(training.root_dir)),
+            trained_model_path=Path(str(training.trained_model_path)),
+            updated_base_model_path=Path(str(prepare_base_model.updated_base_model_path)),
+            training_data=Path(str(training_data)),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+
+
+
+
 
 
     

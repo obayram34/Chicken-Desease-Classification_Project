4a3fe3f0044c25f86b50964cba4fc75db2283f52
obayram34 <oralbayram28@gmail.com>
2023-11-04 03:50:29 +0300

    Evaluation Stage Added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,TrainingConfig,PrepareBaseModelConfig,PrepareCallbacksConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,TrainingConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,EvaluationConfig)
 
 
 
@@ -93,6 +93,18 @@ class ConfigurationManager:
         )
 
         return training_config
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config= EvaluationConfig(
+            path_of_model= "artifacts/training/model.h5",
+            training_data= "artifacts/data_ingestion/Chicken-fecal-images",
+            all_params= self.params,
+            params_image_size= self.params.IMAGE_SIZE,
+            params_batch_size= self.params.BATCH_SIZE
+        )
+        return eval_config
 
 
 
